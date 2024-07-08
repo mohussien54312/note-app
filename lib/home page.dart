b287f1aa90411d921ec725e8ac23c4bf4207b69f
mohussien54312 <mohamedhussien543125@gmail.com>
2024-07-08 04:50:50 +0300

    تاني اخر تعديل

--- lib/home page.dart
+++ lib/home page.dart
@@ -1,7 +1,11 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:note_app/Models/note_model.dart';
+import 'package:note_app/Models/provider_note_model.dart';
+import 'package:note_app/main.dart';
+import 'package:provider/provider.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'Component.dart';
 
 
@@ -13,115 +17,198 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   GlobalKey<FormState> ke = GlobalKey();
   AutovalidateMode v = AutovalidateMode.disabled;
+  TextEditingController title = TextEditingController();
+  TextEditingController subtitle = TextEditingController();
+  bool isloading = false ;
+  bool isswitched = true ;
 
+
+  something(){
+    setState(() {
+      isloading = false;
+    });
+    Navigator.pop(context);
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: Container(
-        decoration: BoxDecoration(
-            color: Colors.grey.withOpacity(0.7),
-            borderRadius: BorderRadius.all(Radius.circular(50))
-        ),
-        child: IconButton(
-          onPressed: (){
-            v=AutovalidateMode.disabled;
-
-           showModalBottomSheet(
-               context: context,
-               builder:(context)=> Container(
-                   padding: EdgeInsets.only(top: 30,right: 20,left: 20,),
-                   child:Form(
-                     key: ke,
-                     child: Column(
-                       children: [
-                         CustomTextField(hint: 'title',validate: v),
-                         Spacer(
-                           flex: 1,
-                         ),
-                         CustomTextField(hint: 'content',max: 5,validate: v,),
-                         Spacer(
-                           flex: 10,
-                         ),
-
-                         Container(
-                           padding: EdgeInsets.symmetric(horizontal: 60),
-                           width: double.infinity,
-
-                           decoration: BoxDecoration(
-                               color: Colors.orangeAccent[100],
-                               borderRadius: BorderRadius.circular(20)
-
-                           ),
-                           child: TextButton(
-                               onPressed: (){
-
-                                 if(ke.currentState!.validate()){
-
-                                 }
-                                 else{
-                                   v=AutovalidateMode.always;
-                                 }
-
-                               },
-                               child: Text('Add',
-                                 style: TextStyle(
-                                     color: Colors.black87
-                                 ),)),
-                         ),
-                         Spacer(
-                           flex: 3,
-                         )
-
-                       ],
-                     ),
-                   )
-               ));
-
-          } ,
-          icon: Icon(Icons.add),
-          iconSize: 55,
-          color: Colors.white,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 70,left: 30,right: 20,bottom: 30),
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(bottom: 24),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Notes",
-                  style: TextStyle(
-                    fontSize: 35,
-                  ),),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.1),
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
-                    child: IconButton(
-                        onPressed: (){},
-                        icon: Icon(Icons.search,size: 30,color: Colors.white,)),
-                  )
-                ],
-              ),
+    final prov = Provider.of<p>(context);
+    return ChangeNotifierProvider(
+      create: (context) => Note(),
+      child: Scaffold(
+        floatingActionButton: Consumer<Note>(
+          builder: (context, value, child) => Container(
+            decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.7),
+                borderRadius: BorderRadius.all(Radius.circular(50))
             ),
-            Expanded(
-              child: ListView.separated(
-                padding: EdgeInsets.only(top: 10),
-                  itemBuilder:(context,index)=>CustomNote(),
-                   itemCount: 6,
-                separatorBuilder: (BuildContext context, int index) =>SizedBox(height: 15,),
-
-              ),
-            )
+            child: IconButton(
+              onPressed: (){
+                v=AutovalidateMode.disabled;
 
 
+                showModalBottomSheet(
+                    isScrollControlled: true,
+                    showDragHandle: true,
+
+                    constraints: BoxConstraints(
+
+                      maxHeight: 700
+                    ),
+                    context: context,
+                    builder:(context)=> Container(
+                      constraints: BoxConstraints(
 
 
-          ],
+                      ),
+                        padding: EdgeInsets.only(top: 30,right: 20,left: 20,bottom: MediaQuery.of(context).viewInsets.bottom),
+                        child:Form(
+                          key: ke,
+                          child: Column(
+                            children: [
+                              CustomTextField(hint: 'title',validate: v,controller: title,),
+                              Spacer(
+                                flex: 1,
+                              ),
+                              CustomTextField(hint: 'content',max: 5,validate: v,controller: subtitle,),
+                              Spacer(
+                                flex: 10,
+                              ),
+
+                              Container(
+                                padding: EdgeInsets.symmetric(horizontal: 60),
+                                width: double.infinity,
+
+                                decoration: BoxDecoration(
+                                    color: Colors.orangeAccent[100],
+                                    borderRadius: BorderRadius.circular(20)
+
+                                ),
+                                child: TextButton(
+                                    onPressed: (){
+
+                                      if(ke.currentState!.validate()){
+
+
+                                        try {
+                                                                                      value.addNote(NoteModel(title.text, subtitle.text,DateTime.now().toString()));
+
+
+                                        } on Exception catch (e) {
+                                          setState(() {
+                                            isloading = false;
+                                          });
+                                          print(e);
+                                        }
+                                        Future.delayed(Duration(
+                                          seconds: 3
+                                        ),
+                                        () => something()
+                                          ,);
+
+                                      }
+                                      else{
+                                        v=AutovalidateMode.always;
+                                      }
+
+                                    },
+                                    child: value.loading==true ? CircularProgressIndicator(): Text('Add',
+                                      style: TextStyle(
+                                          color: Colors.black87
+                                      ),)),
+                              ),
+                              Spacer(
+                                flex: 3,
+                              )
+
+                            ],
+                          ),
+                        )
+                    ));
+
+              } ,
+              icon: Icon(Icons.add),
+              iconSize: 55,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Consumer<Note>(
+          builder: (context, value, child) => Padding(
+            padding: const EdgeInsets.only(top: 70,left: 30,right: 20,bottom: 30),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(bottom: 24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Notes",
+                        style: TextStyle(
+                          fontSize: 35,
+                        ),),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey.withOpacity(0.1),
+                            borderRadius: BorderRadius.all(Radius.circular(10))
+                        ),
+                        child: IconButton(
+                            onPressed: (){},
+                            icon: Icon(Icons.search,size: 30,color: Colors.white,)),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Switch(
+                        value: isswitched,
+                        onChanged: (b){
+                          setState(() {
+                            isswitched = !isswitched;
+                            prov.changetheme();
+                          });
+                        })
+                  ],
+                ),
+                Expanded(
+                  child: FutureBuilder(
+                    builder: (context,snapshot){
+                      if(snapshot.hasData)
+                      return  ListView.separated(
+                        reverse: true,
+                        padding: EdgeInsets.only(top: 10),
+                        itemBuilder:(context,index)=>CustomNote(Title:snapshot.data![index].Title,Subtitle:snapshot.data![index].SubTitle ,Date:snapshot.data![index].Date ,delete: (){
+                          value.deleteNote(index);
+                        },),
+                        itemCount: snapshot.data!.length,
+                        separatorBuilder: (BuildContext context, int index) =>SizedBox(height: 15,),
+
+                      );
+                      else if (snapshot.data ==null){
+                        return
+                        Center(
+                          child: Text("hiiiii"),
+                        );
+                      }
+                      else{
+                        return   Center(
+                          child: Text("hi there"),
+                        );
+                      }
+                    },
+                    future: value.fetchNotes(),
+
+                  ),
+                )
+
+
+
+
+              ],
+            ),
+          ),
+
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/Models/note_model.dart';
+import 'package:provider/provider.dart';
 
 import 'EditNote.dart';
 import 'home page.dart';
@@ -10,24 +12,81 @@ import 'home page.dart';
 void main() async{
 
   await Hive.initFlutter();
-  await Hive.openBox('note box');
-  runApp(const MyApp());
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>('note box');
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context)=>p())
+    ],
+      child: const MyApp()));
 
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       routes: {
         'EditePage' :  (context)=>EditeNotes(),
       },
-      theme: ThemeData(
-        brightness: Brightness.dark
-      ),
+      theme:Provider.of<p>(context).theme,
       home: HomePage(),);
   }
+}
+
+
+
+
+
+
+class ThemeStyle {
+
+  static ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      primary: Colors.black,
+      secondary: Colors.white70,
+      background: Colors.white70
+    )
+  );
+
+
+  static ThemeData dark = ThemeData(
+      brightness: Brightness.dark,
+      colorScheme: ColorScheme.dark(
+          primary: Colors.orangeAccent,
+          secondary: Colors.grey.shade900,
+          background: Colors.white10
+      )
+  );
+}
+
+
+
+class p extends ChangeNotifier{
+
+  ThemeData theme = ThemeStyle.light;
+
+   changetheme(){
+
+    if(theme==ThemeStyle.light)
+      theme = ThemeStyle.dark;
+
+    else{
+      theme =ThemeStyle.light;
+    }
+    notifyListeners();
+  }
+
+
 }
